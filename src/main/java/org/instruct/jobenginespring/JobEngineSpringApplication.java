5e9fde8e287aa80bc1504eb8022ee057e78e30f7
Joni Hysaj <75336980+JonHHH09@users.noreply.github.com>
2026-06-29 21:51:22 -0400

    init

---
+++ src/main/java/org/instruct/jobenginespring/JobEngineSpringApplication.java
@@ -0,0 +1,13 @@
+package org.instruct.jobenginespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobEngineSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobEngineSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/instruct/jobenginespring/JobEngineSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.instruct.jobenginespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobEngineSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
